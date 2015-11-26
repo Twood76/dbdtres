2a35c87e1107239f8c11d9868acf0ca248157203
Paul Dante <Paul.Dante@Dynamic-Leap.com>
2015-11-26 14:27:23 -0800

    Started on MagicTypes

--- app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/MagicTypes.java
+++ app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/MagicTypes.java
@@ -8,8 +8,34 @@ import com.dante.paul.dd5erandomlootgenerator.Dice.Dice;
 public class MagicTypes extends ValueableItems {
     Dice d = new Dice();
     int number;
+    String magicItem;
+    //TODO randomly generate spells of given level
+    public String getItem(String table){
+        number = d.roll(100);
+        switch(table){
+            case "A":
+                if (number<51){
+                    magicItem = "Potion of healing";
+                }else if (number<61){
+                    magicItem = "Spell Scroll (cantrip)";
+                }else if (number<71){
+                    magicItem = "Potion of climbing";
+                }else if(number<91){
+                    magicItem = "Spell Scroll (1st Level)";
+                }else if(number<95){
+                    magicItem = "Spell Scroll (2nd Level)";
+                }else if (number<99){
+                    magicItem = "Potion of greater healing";
+                }else if (number==99){
+                    magicItem = "Bag of holding";
+                }else{
+                    magicItem = "Driftglobe";
+                }
+                return magicItem;
+            //TODO create other magic item tables
+            default:
 
-    @Override
-    public String getItem(){
+                return magicItem;
+        }
 }
 }
