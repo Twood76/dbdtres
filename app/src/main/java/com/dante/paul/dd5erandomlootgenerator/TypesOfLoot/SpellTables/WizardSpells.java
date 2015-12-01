6e9f74a91ccef13aacd699d6e53e7b6343ff5117
pdante <Paul.Dante@Dynamic-Leap.com>
2015-11-30 18:04:00 -0800

    Finished Wizard. All spell lists done.

--- app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/SpellTables/WizardSpells.java
+++ app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/SpellTables/WizardSpells.java
@@ -7,15 +7,14 @@ public class WizardSpells extends AbstractSpells implements Spells {
 
     public WizardSpells() {
     }
-    private String[] level0 = {};
-    private String[] level1 = {};
-    private String[] level2 = {};
-    private String[] level3 = {};
-    private String[] level4 = {};
-    private String[] level5 = {};
-    private String[] level6 = {};
-    private String[] level7 = {};
-    private String[] level8 = {};
-    private String[] level9 = {};
-    //TODO spell levels 0-9
+    private String[] level0 = {"Acid Splash", "Blade Ward\", \" Chill Touch\", \" Dancing Lights\", \" Fire Bolt\", \" Friends\", \" Light\", \" Mage Hand\", \" Mending\", \" Message\", \" Minor Illusion\", \" Poison Spray\", \" Prestidigitation\", \" Ray of Frost\", \" Shocking Grasp\", \" True Strike"};
+    private String[] level1 = {"Alarm\", \" Burning Hands\", \" Charm Person\", \" Chromatic Orb\", \" Color Spray\", \" Comprehend Languages\", \" Detect Magic\", \" Disguise Self\", \" Expeditious Retreat\", \" False Life\", \" Feather Fall\", \" Find Familiar\", \" Fog Cloud\", \" Grease\", \" Identify\", \" Illusory Script\", \" Jump\", \" Longstrider\", \" Mage Armor\", \" Magic Missile\", \" Protection from Evil and Good\", \" Ray of Sickness\", \" Shield\", \" Silent Image\", \" Sleep\", \" Tasha's Hideous Laughter\", \" Tenser's Floating Disk\", \" Thunderwave\", \" Unseen Servant\", \" Witch Bolt"};
+    private String[] level2 = {"Alter Self\", \" Arcane Lock\", \" Blindness/Deafness\", \" Blur\", \" Cloud of Daggers\", \" Continual Flame\", \" Crown of Madness\", \" Darkness\", \" Darkvision\", \" Detect Thoughts\", \" Enlarge/Reduce\", \" Flaming Sphere\", \" Gentle Repose\", \" Gust of Wind\", \"Hold Person\", \" Invisibility\", \" Knock\", \" Levitate\", \" Locate Object\", \" Magic Mouth\", \" Magic Weapon\", \" Melf's Acid Arrow\", \" Mirror Image\", \" Misty Step\", \" Nystul's Magic Aura\", \" Phantasmal Force\", \" Ray of Enfeeblement\", \" Rope Trick\", \" Scorching Ray\", \" See Invisibility\", \" Shatter\", \" Spider Climb\", \" Suggestion\", \" Web" };
+    private String[] level3 = {"Animate Dead\", \" Bestow Curse\", \" Blink\", \" Clairvoyance\", \" Counterspell\", \" Dispel Magic\", \" Fear\", \" Feign Death\", \" Fireball\", \" Fly\", \" Gaseous Form\", \" Glyph of Warding\", \" Haste\", \" Hypnotic Pattern\", \" Leomund's Tiny Hut\", \" Lightning Bolt\", \" Magic Circle\", \" Major Image\", \" Nondetection\", \" Phantom Steed\", \" Protection from Energy\", \" Remove Curse\", \" Sending\", \" Sleet Storm\", \" Slow\", \" Stinking Cloud\", \" Tongues\", \" Vampiric Touch\", \" Water Breathing"};
+    private String[] level4 = {"Arcane Eye\", \" Banishment\", \" Blight\", \" Confusion\", \" Conjure Minor Elementals\", \" Control Water\", \" Dimension Door\", \" Evard's Black Tentacles\", \" Fabricate\", \" Fire Shield\", \" Greater Invisibility\", \" Hallucinatory Terrain\", \" Ice Storm\", \" Leomund's Secret Chest\", \" Locate Creature\", \" Mordenkainen's Failhful Hound\", \" Mordenkainen's Private Sanctum\", \" Otiluke's Resilient Sphere\", \" Phantasmal Killer\", \" Polymorph\", \" Stone Shape\", \" Stoneskin\", \" Wall of Fire"};
+    private String[] level5 = {"Animate Objects\", \" Bigby's Hand\", \" Cloudkill\", \" Cone of Cold\", \" Conjure Elemenlal\", \" Contact Other Plane\", \" Creation\", \" Dominate Person\", \" Dream\", \" Geas\", \" Hold Monster\", \" Legend Lore\", \" Mislead\", \" Modify Memory\", \" Passwall\", \" Planar Binding\", \" Rary's Telepathic Bond\", \" Scrying\", \" Seeming\", \" Telekinesis\", \" Teleportation\", \" Wall of Force\", \" Wall of Stone"};
+    private String[] level6 = {"Arcane Gate\", \" Chain Lightning\", \" Circle of Death\", \" Contingeney\", \" Create Undead\", \" Disintegrate\", \" Drawmij's Instant Summons\", \" Eyebite\", \" Flesh to Stone\", \" Globe of Invulnerability\", \" Guards and Wards\", \" Magic Jar\", \" Mass Suggestion\", \" Move Earth\", \" Otiluke's Freezing Sphere\", \" Otto's Irresislible Dance\", \" Programmed Illusion\", \" Sunbeam\", \" True Seeing\", \" Wall of Ice"};
+    private String[] level7 = {"Delayed Blast\", \" Fireball\", \" Etherealness\", \" Finger of Death\", \" Forcecage\", \" Mirage Arcane\", \" Mordenkainen's Magnificent Mansion\", \" Mordenkainen's Plane Shift\", \" Prismatic Spray\", \" Project Image\", \" Reverse Gravity\", \" Sequester\", \" Simulacrum\", \" Symbol\", \" Teleport"};
+    private String[] level8 = {"Antimagic Field\", \" Antipathy/Sympathy\", \" Clone\", \" Control Weather\", \" Demiplane\", \" Dominate Feeblemind\", \" Incendiary Cloud\", \" Maze\", \" Mind Blank\", \" Power Word Stun\", \" Sunburst\", \" Telepathy\", \" Trap the Soul"};
+    private String[] level9 = {"Astral Projection\", \" Foresight\", \" Gale\", \" Imprisonment\", \" Meteor Swarm\", \" Power Word Kill\", \" Prismatic Wall\", \" Shapechange\", \" Time Stop\", \" True Polymorph\", \" Weird\", \" Wish"};
 }
