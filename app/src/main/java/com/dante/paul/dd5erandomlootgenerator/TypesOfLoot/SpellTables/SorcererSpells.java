688921f1bec99daf5d5c5ef353ece3d83e42569f
pdante <Paul.Dante@Dynamic-Leap.com>
2015-11-30 17:33:03 -0800

    Finished Sorcerer and Warlock Spell tables

--- app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/SpellTables/SorcererSpells.java
+++ app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/SpellTables/SorcererSpells.java
@@ -7,15 +7,14 @@ public class SorcererSpells extends AbstractSpells implements Spells {
 
     public SorcererSpells() {
     }
-    private String[] level0 = {};
-    private String[] level1 = {};
-    private String[] level2 = {};
-    private String[] level3 = {};
-    private String[] level4 = {};
-    private String[] level5 = {};
-    private String[] level6 = {};
-    private String[] level7 = {};
-    private String[] level8 = {};
-    private String[] level9 = {};
-    //TODO spell levels 0-9
+    private String[] level0 = {"Acid Splash", "Blade Ward", "Chill Touch", "Dancing Lights", "Fire Bolt", "Friends", "Light", "Mage Hand", "Mending", "Message", "Minor Illusion", "Poison Spray", "Prestidigitation", "Ray of Frost", "Shocking Grasp", "True Strike" };
+    private String[] level1 = {"Burning Hands", "Charm Person", "Chromatic Orb", "Color Spray", "Comprehend", "Detect Magic", "Disguise Self", "Expeditious Retreat", "False Life", "Feather Fali", "Fog Cloud", "Jump", "Mage Armor", "Magic Missile", "Ray of Sickness", "Shield", "Silent Image", "Sleep", "Thunderwave", "Witch Bolt"};
+    private String[] level2 = {"Alter Self", "Blindness/Deafness", "Blur", "Cloud of Daggers", "Crown of Madness", "Darkness", "Darkvision", "Detect Thoughts", "Enhance Ability", "Enlarge/Reduce", "Gust of Wind", "Hold Person", "Invisibility", "Knock", "Levitate", "Mirror Image", "Misty Step", "Phantasmal Force", "Scorching Ray", "See Invisibility", "Shatter", "Spider Climb", "Suggestion", "Web"};
+    private String[] level3 = {"Blink", "Clairvoyance", "Counterspell", "Daylight", "Dispel Magic", "Fear", "Fireball", "Fly", "Gaseous Form", "Haste", "Hypnotic Pattern", "Lightning Bolt", "Major Image", "Protection from Energy", "Sleet Storm", "Slow", "Stinking Cloud", "Tongues", "Water Breathing", "Water Walk"};
+    private String[] level4 = {"Banishment", "Blight", "Confusion", "Dimension Door", "Dominate Beast", "Greater Invisibility", "Ice Storm", "Polymorph", "Stoneskin", "Wall of Fire"};
+    private String[] level5 = {"Animate Objects", "Cloudkill", "Cone of Cold", "Creation", "Dominate Person", "Hold Monster", "Insect Plague", "Seeming", "Telekinesis", "Teleportation Cirele", "Wall of Stone"};
+    private String[] level6 = {"Arcane Gate", "Chain Lightning", "Circle of Death", "Disintegrate", "Eyebite", "Globe of Invulnerability", "Mass Suggestion", "Move Earth", "Sunbeam", "True Seeing"};
+    private String[] level7 = {"Delayed Blast", "Fireball", "Etherealness", "Finger of Death", "Fire Storm", "Plane Shift", "Prismatic Spray", "Reverse Gravity", "Teleport"};
+    private String[] level8 = {"Dominate", "Earthquake", "Incendiary Cloud", "Power Word Stun", "Sunburst"};
+    private String[] level9 = {"Gate Meteor", "Swarm", "Power Word Kill", "Time Stop", "Wish"};
 }
--- app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/SpellTables/WarlockSpells.java
+++ app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/SpellTables/WarlockSpells.java
@@ -7,15 +7,14 @@ public class WarlockSpells extends AbstractSpells implements Spells {
 
     public WarlockSpells() {
     }
-    private String[] level0 = {};
-    private String[] level1 = {};
-    private String[] level2 = {};
-    private String[] level3 = {};
-    private String[] level4 = {};
-    private String[] level5 = {};
-    private String[] level6 = {};
-    private String[] level7 = {};
-    private String[] level8 = {};
-    private String[] level9 = {};
-    //TODO spell levels 0-9
+    private String[] level0 = {"Blade Ward", "Chill Touch", "Eldritch Blast", "Friends", "Mage Hand", "Minor Illusion", "Poison Spray", "Prestidigitation True Strike"};
+    private String[] level1 = {"Armor of Agathys", "Arms of Hadar", "Charm Person", "Comprehend Languages", "Expeditious Retreat", "Hellish Rebuke", "Hex", "Illusory Script", "Protection from Evil and Good", "Unseen Servant", "Witch Bolt"};
+    private String[] level2 = {"Cloud of Daggers", "Crown of Madness", "Darkness", "Enthrall", "Hold Person", "Invisibility", "Mirror Image", "Misty Step", "Ray of Enfeeblement", "Shatter", "Spider Climb", "Suggestion"};
+    private String[] level3 = {"Counterspell", "Dispel Magic", "Fear", "Fly", "Gaseous Form", "Hunger of Hadar", "Hypnotic Pattern", "Magic Circle", "Major Image", "Remove Curse", "Tongues", "Vampiric Touch"};
+    private String[] level4 = {"Banishment", "Blight", "Dimension Door", "Hallucinatory Terrain"};
+    private String[] level5 = {"Contact Other Plane", " Dream", " Hold Monster", " Scrying"};
+    private String[] level6 = {"Arcane Gate", "Circle of Death", "Conjure Fey", "Create Undead", "Eyebite", "Flesh to Stone", "Mass Suggestion", "True Seeing"};
+    private String[] level7 = {"Etherealness", "Finger of Death", "Forcecage", "Plane Shift"};
+    private String[] level8 = {"Demiplane", "Dominate Monster", "Feeblemind", "Glibness", "Power Word Stun"};
+    private String[] level9 = {"Astral Projection", "Foresight", "Imprisonment", "Power Word Kill", "True Polymorph"};
 }
