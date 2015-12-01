488d397dfb3bb0d03a44acd704525261a852814c
pdante <Paul.Dante@Dynamic-Leap.com>
2015-11-30 16:59:58 -0800

    Finished Druid Spells

--- app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/SpellTables/DruidSpells.java
+++ app/src/main/java/com/dante/paul/dd5erandomlootgenerator/TypesOfLoot/SpellTables/DruidSpells.java
@@ -7,15 +7,14 @@ public class DruidSpells extends AbstractSpells implements Spells {
 
     public DruidSpells() {
     }
-    private String[] level0 = {};
-    private String[] level1 = {};
-    private String[] level2 = {};
-    private String[] level3 = {};
-    private String[] level4 = {};
-    private String[] level5 = {};
-    private String[] level6 = {};
-    private String[] level7 = {};
-    private String[] level8 = {};
-    private String[] level9 = {};
-    //TODO spell levels 0-9
+    private String[] level0 = {"Druidcraft", "Guidance", "Mending", "Poison Spray", "Produce Flame", "Resistance", "Shillelagh", "Thorn Whip"};
+    private String[] level1 = {"Animal Friendship", "Charm Person", "Create or Destroy Water", "Cure Wounds", "Detect Magic", "Detect Poison and Disease", "Entangle", "Faerie Fire", "Fog Cloud", "Goodberry", "Healing Word", "Jump", "Longstrider", "Purify Food and Drink", "Speak with Animals", "Thunderwave"};
+    private String[] level2 = {"Animal Messenger", "Barkskin", "Beast Sense", "Darkvision", "Enhance Ability", "Find Traps", "Flame Blade", "Flaming Sphere", "Gust of Wind", "Heat Metal", "Hold Person", "Lesser Restoration", "Locate Animals or Plants", "Locate Object", "Moonbeam", "Pass without Trace", "Protection from Poison", "Spike Growth"};
+    private String[] level3 = {"Call Lightning", "Conjure Animals", "Daylight", "Dispel Magic", "Feign Death", "Meld into Stone", "Plant Growth", "Protection from Energy", "Sleet Storm", "Speak with Plants", "Water Breathing", "Water Walking", "Wind Wall"};
+    private String[] level4 = {"Blight", "Confusion", "Conjure Minor Elemental", "Conjure Woodland Being", "Control Water", "Dominate Beast", "Freedom of Movement", "Giant Insect", "Grasping Vine", "Hallucinatory Terrain", "Ice Storm", "Locate Creature", "Polymorph", "Stone Shape", "Stoneskin", "Wall of Fire"};
+    private String[] level5 = {"Antilife Shell", "Awaken", "Commune with Nature", "Conjure Elemental", "Contagion", "Geas", "Greater Restoration", "Insect Plague", "Mass Cure Wounds", "Planar Binding", "Reincarnate", "Scrying", "Tree Stride", "Wall of Stone"};
+    private String[] level6 = {"Conjure Fey", "Find the Path", "Heal", "Heroes' Feast", "Move Earth", "Sunbeam", "Transport via Plants", "Wall of Thorns", "Wind Walk"};
+    private String[] level7 = {"Fire Storm", "Mirage Arcane", "Plane Shift", "Regenerate", "Reverse Gravity"};
+    private String[] level8 = {"Animal Shapes", "Antipathy/Sympathy", "Control Weather", "Earthquake", "Feeblemind", "Sunburst", "Tsunami"};
+    private String[] level9 = {"Foresight", "Shapechange", "Storm of Vengence", "True Resurrection"};
 }
