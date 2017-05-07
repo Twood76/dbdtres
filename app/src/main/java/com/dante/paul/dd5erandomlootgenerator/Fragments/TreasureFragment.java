4f29a5f7f3444d1b8a7baf2c3f83a087395c8d29
Paul Dante <pdante@Paul-Desky>
2017-05-07 13:49:20 -0700

    Updated gradle and removed ads

--- app/src/main/java/com/dante/paul/dd5erandomlootgenerator/Fragments/TreasureFragment.java
+++ app/src/main/java/com/dante/paul/dd5erandomlootgenerator/Fragments/TreasureFragment.java
@@ -54,9 +54,6 @@ public class TreasureFragment extends Fragment{
             }
         });
 
-        AdView mAdView = (AdView) view.findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
     return view;
     }
 
